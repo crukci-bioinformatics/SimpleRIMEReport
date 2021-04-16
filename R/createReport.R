06cd4f3baafb1ea2482e5bf2f09a1c88fbf0e6d6
Ashley Sawle <ads2202cu@gmail.com>
2021-04-16 14:12:53 +0100

    turn into a function and add roxygen

--- R/createReport.R
+++ R/createReport.R
@@ -1,104 +1,45 @@
-library(GenomicRanges)
-library(Biostrings)
-library(tidyverse)
-library(openxlsx)
-library(cowplot)
-# Get functions
-source("createSampleTable.R")
-source("loadDataFunctions.R")
-source("plotFunctions.R")
-source("createWorkBookFunctions.R")
+#' Create the Simple RIME report
+#'
+#' This function generates report as an Excel workbook. The input is a sample
+#' table.
+#'
+#' @param sampleTable data.frame; the sample table
+#' @param dataDir character; Path to directory containing the Proteome
+#'   Discoverer output files
+#' @return NULL
+#' @examples
+#'
+#' @import GenomicRanges
+#' @import Biostrings
+#' @import openxlsx
+#' @import cowplot
+#' @import tidyverse
+#'
+#' @export createReport
 
-################################################################################
-# 1. Set Input Parameters
-################################################################################
+createReport <- function(sampleTable, dataDir){
 
-data_directory <- "../data"
-species_id <- as.numeric(9606)
-# output file name
-outputFileName <- str_c(data_directory, "/", 
-                        basename(data_directory), ".report.xlsx")
-
-################################################################################
-# 2. Generate sample table
-# This portion of the scripts uses the file names to generate a sample table
-# Run the script and then open the table it saves in Excel and make any 
-# changes or additions as needed.
-# The table will be saved in the 'data_directory' as "XXXXX_sample_details.csv"
-# The sample table will contain 5 columns:
-# ProteinFile - the file name for 'protiengroups' data
-# PeptideFile - the file name for 'psms' data
-# SampleName - the name for the sample - this is used for the plot titles and
-#              the names of the worksheets
-# BaitProteinName - the protein name for the bait protein, e.g. ER, FOXA1 etc. 
-#                   This is only used in the plot titles, it could be anything
-# BaitProteinUniProtID - the UniProt ID for the bait protein, e.g. P03372, P55317
-#                       this is used to look up the fasta sequence
-# The script will attempt to guess the SampleName and BaitProteinName from the
-# the protein file name.
-# 
-# The assumed structure of the protein file name is:
-# QE_HF_<Date>_<PR_number>_<Initials>_<SampleName>_<BaitProteinName>_proteingroups.txt
-# SampleName can include "_"
-# e.g. 
-#   QE_HF_04092018_PR1025_SAN_Sample1_VEH_ARID2_proteingroups.txt
-# gives
-#   SampleName="Sample1_VEH"
-#   BaitProteinName="ARID2"
-#
-# You'll need to check the sample sheet and modify as necessary before 
-# proceeding
-################################################################################
-
-createSampleTable(data_directory)
-
-################################################################################
-# 3. Generate the report
-# Once you have adjusted the sample table run the rest of the code to generate
-# the report
-################################################################################
-
-# get sample table
-sampleTable <- str_c(data_directory, "/", 
-                     basename(data_directory), "_sample_details.csv") %>% 
-    read_csv(col_types = cols()) %>% 
-    checkBaitDetection(data_directory)
-
-if(all(str_detect(sampleTable$BaitProteinName, "^[Ii][Gg][Gg]$"))){
-    stop("There are no samples in which the bait protein provided was detected.")
+    # Load from preconstructed SwissProt file
+    annotTab <- makeAnnotation(sampleTable, dataDir)
+    
+    # Get fasta seq from SwissProt file 
+    fastaSeqTable <- createFastaTable(sampleTable)
+    
+    # load data
+    protein_data <- getProteinTables(sampleTable, dataDir, annotTab)
+    peptide_data <- getPeptideTables(sampleTable, dataDir)
+    
+    # make merged tables
+    combined_data <- combineProteinTables(sampleTable, dataDir, annotTab)
+    filtered_data <- filterCombinedTable(sampleTable, dataDir, annotTab)
+    
+    # make plots
+    coveragePlots <- makeCoveragePlots(sampleTable, peptide_data, fastaSeqTable)
+    
+    # make workbook
+    makeWorkBook(outputFileName,
+                       coveragePlots,
+                       protein_data,
+                       combined_data,
+                       filtered_data)
 }
-
-# make an annotation table.
-# For each acc get annotations from UniProt
-# uniProtConnection <- UniProt.ws(taxId = species_id)
-# annotTab <- loadAnnotation(sampleTable, 
-#                         data_directory, 
-#                         species_id, 
-#                         uniProtConnection)
-# Get fasta seq from Uniprot
-# fastaSeqTable <- makeFastaTable(sampleTable, species_id, uniProtConnection)
-
-# make an annotation table.
-# Load from preconstructed SwissProt file
-annotTab <- makeAnnotation(sampleTable, data_directory)
-
-# Get fasta seq from SwissProt file 
-fastaSeqTable <- createFastaTable(sampleTable)
-
-# load data
-protein_data <- getProteinTables(sampleTable, data_directory, annotTab)
-peptide_data <- getPeptideTables(sampleTable, data_directory)
-
-# make merged tables
-combined_data <- combineProteinTables(sampleTable, data_directory, annotTab)
-filtered_data <- filterCombinedTable(sampleTable, data_directory, annotTab)
-
-# make plots
-coveragePlots <- makeCoveragePlots(sampleTable, peptide_data, fastaSeqTable)
-
-# make workbook
-makeWorkBook(outputFileName,
-                   coveragePlots,
-                   protein_data,
-                   combined_data,
-                   filtered_data)
