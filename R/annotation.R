51e22052cc4f268442c5480f99088563a7c22d22
Ashley Sawle <ads2202cu@gmail.com>
2021-04-19 19:15:19 +0100

    Update to use internal data object

--- R/annotation.R
+++ R/annotation.R
@@ -1,22 +1,16 @@
-loadAnnotation <- function(sampleTab, dataDir, species_id, upCon){
-    columns <- c("ENTRY-NAME", "PROTEIN-NAMES", "GENES")
-    loadProteinData(sampleTab, dataDir) %>%   
-        map(pull, var=1) %>%
-        unlist() %>%
-        unique() %>%
-        UniProt.ws::select(upCon, ., columns, "UNIPROTKB") %>%
-        as_tibble() %>% 
-        mutate(GeneSymbol = gsub(" .*", "", GENES)) %>% 
-        select(Accessions = "UNIPROTKB", Gene = "ENTRY-NAME",
-               Description = "PROTEIN-NAMES", GeneSymbol)
-}
+#' Create annotation table
+#' 
+#' Creates an annotation table for the report
+#' @name makeAnnotation
+#' 
+#' @import dplyr
+#' @import purrr
+#' @import readr
 
 makeAnnotation <- function(sampleTab, dataDir){
-    str_c(dataDir, "/", sampleTab$ProteinFile) %>% 
-        map_df(read_tsv, col_types = cols()) %>%  
+    map_df(sampleTab$ProteinFile, read_tsv, col_types = cols()) %>%  
         select(Accessions=Accession) %>%  
         distinct() %>%   
-        left_join(readRDS("annotation/SwissProtAnnotationTable.rds"), 
-                  by="Accessions") %>% 
+        left_join(annot, by="Accessions") %>% 
         select(-Sequence)
 }
