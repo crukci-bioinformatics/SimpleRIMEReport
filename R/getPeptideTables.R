a900ce7d11c0ea4b4f11c9293788514d040c874f
Ashley Sawle <ads2202cu@gmail.com>
2021-04-20 09:44:26 +0100

    Document and update for interanl annotation

--- R/getPeptideTables.R
+++ R/getPeptideTables.R
@@ -1,7 +1,11 @@
-
-# load peptide data. Each table into a element of a list
-getPeptideTables <- function(sampleTab, dataDir){
-    str_c(dataDir, "/", sampleTab$PeptideFile) %>% 
-        map(read_tsv, col_types = cols()) %>% 
+#' Get peptide tables
+#' 
+#' This function reads each Peptide data file and creates list of peptide level 
+#' data tables -  one for each bait and the control.
+#' @name getPeptideTables
+#' @import readr
+#' @import purrr
+getPeptideTables <- function(sampleTab){
+    map(sampleTab$PeptideFile, read_tsv, col_types = cols()) %>% 
         set_names(sampleTab$SampleName)
 }
--- R/getProteinTables.R
+++ R/getProteinTables.R
@@ -1,7 +1,14 @@
-
-getProteinTables <- function(sampleTab, dataDir, annot){
-    loadProteinData(sampleTab, dataDir) %>%   
+#' Get protein tables
+#' 
+#' This function reads each Protein data file and creates list of protein level 
+#' data tables with annotations -  one for each bait and the control.
+#' @name getProteinTables
+#' @import dplyr
+#' @import readr
+#' @import purrr
+getProteinTables <- function(sampleTab){
+    loadProteinData(sampleTab) %>%   
         map(left_join, annot, by=c("Accession"="Accessions")) %>% 
         map(select, Accession, Gene, `Gene Symbol`=GeneSymbol, 
-            Description, everything())
+            Description, everything(), -Sequence)
 }
