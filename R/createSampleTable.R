1a2b334265bb7ecf25dced0327c46cad536be7f4
Ashley Sawle <ads2202cu@gmail.com>
2021-04-26 12:44:42 +0100

    Update man page, add error checking for files, add message about save location

--- R/createSampleTable.R
+++ R/createSampleTable.R
@@ -20,9 +20,12 @@ filenametotable <- function(fileName){
 #' Create the sample table based on files in the data directory
 #'
 #' This function scans the contents of a directory for the presence of Proteome
-#' Discoverer results files. The files required for each sample are a Protein
-#' table and a Peptide table. The filenames are expected to have the following
-#' naming convention:
+#' Discoverer results files. 
+#'
+#' @param dataDir character; Path to directory containing the Proteome
+#'   Discoverer output files
+#' @details The files required for each sample are a Protein table and a Peptide table.
+#' The filenames are expected to have the following naming convention:
 #'
 #' QE_HF_<Date>_<PR_number>_<Initials>_<SampleName>_<BaitProteinName>_Proteins.txt
 #' QE_HF_<Date>_<PR_number>_<Initials>_<SampleName>_<BaitProteinName>_PeptideGroups.txt
@@ -31,31 +34,33 @@ filenametotable <- function(fileName){
 #' sample table, which is used to generate the report. 
 #' 
 #' The sample table will contain 5 columns:
-#' ProteinFile - the file name for 'protiengroups' data
-#' PeptideFile - the file name for 'psms' data
-#' SampleName - the name for the sample - this is used for the plot titles and
-#'              the names of the worksheets
-#' BaitProteinName - the protein name for the bait protein, e.g. ER, FOXA1 etc. 
-#'                   This is only used in the plot titles, it could be anything
-#' BaitProteinUniProtID - the UniProt ID for the bait protein, e.g. P03372, P55317
-#'                       this is used to look up the fasta sequence
+##' \itemize{
+##'  \item{ ProteinFile - the file name for 'protiengroups' data }
+##'  \item{ PeptideFile - the file name for 'psms' data }
+##'  \item{ SampleName - the name for the sample - this is used for the plot
+##'                      titles and the names of the worksheets }
+##'  \item{ BaitProteinName - the protein name for the bait protein, e.g. ER,
+##'                           FOXA1 etc. This is only used in the plot titles,
+##'                           it could be anything }
+##'  \item{ BaitProteinUniProtID - the UniProt ID for the bait protein, e.g.
+##'                                P03372, P55317 this is used to look up the
+##'                                fasta sequence }
+##' }
 #' 
 #' The script will attempt to deduce the SampleName and BaitProteinName from the
 #' the file name. It will also try to determine the UniProt ID from the bait
-#' protein name.
+#' protein name. If the BaitProteinName is associated with multiple UniProt IDs,
+#' the script will return all possibilities.
 #' 
 #' It is important to check the details contained in the table, and modify it if
 #' necessary, prior to generating the report. In particular check the bait
 #' protein name and UniProt ID. The negative control should be called "Control",
 #' "Empty" or "IgG" and have no UniProtID.
-#'
-#' @param dataDir character; Path to directory containing the Proteome
-#'   Discoverer output files
 #' @return A sample table suitable for input into the `createReport` function. A
 #'   copy of the table it also written to the data directory as a comma 
 #'   separated table.
+#' 
 #' @examples
-#'
 #'
 #' @import dplyr
 #' @import purrr
@@ -65,8 +70,14 @@ filenametotable <- function(fileName){
 #'
 #' @export createSampleTable
 createSampleTable <- function(dataDir){
-    tab <- list.files(dataDir, pattern = "_Proteins.txt$") %>%
-        map_df(filenametotable)
+    if(!dir.exists(dataDir)) { 
+        stop("The data directory provided - ", dataDir, " - cannot be found")
+        }
+    protFiles <- list.files(dataDir, pattern = "_Proteins.txt$")
+    if(length(protFiles)==0){
+        stop("No '_Protein.txt' files were found in ", dataDir)
+    }
+    tab <- map_df(protFiles, filenametotable)
     uid <- filter(annot, GeneSymbol%in%tab$BaitProteinName) %>% 
         select(BaitProteinName = GeneSymbol, Accession) %>% 
         group_by(BaitProteinName) %>% 
@@ -75,5 +86,6 @@ createSampleTable <- function(dataDir){
     tab <- left_join(tab, uid, by="BaitProteinName")
     outnam <- str_c(dataDir, "/", basename(dataDir), "_sample_details.csv")
     write_csv(tab, outnam)
+    message("A copy of the sample table has been saved at ", outnam)
     return(tab)
 }
